3cc647d480744efc3964db4b9a4f377d309e78ef
GouthamGuna <ggowthamsankar@gmail.com>
2022-11-20 12:09:45 +0530

    20-Nov-22 / day commit : 1 .

---
+++ learn_java/src/streamAPI/PracticeStreamAPI.java
@@ -0,0 +1,12 @@
+package streamAPI;
+
+import java.util.stream.Stream;
+
+public class PracticeStreamAPI {
+    public static void main(String[] args) {
+        Stream.iterate(1,element->element+1)
+                .filter(element->element %5==0)
+                .limit(10)
+                .forEach(System.out::println);
+    }
+}
