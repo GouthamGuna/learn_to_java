0d5bdb9a5c91077bae30a0ff7512666c5b04fddd
gouthamguna <ggowthamsankar@gmail.com>
2022-12-13 13:18:17 +0530

    StringReverseWord add

---
+++ office_system/suresh_bro_system/practice_java/src/String/StringReverseWord.java
@@ -0,0 +1,16 @@
+package String;
+
+public class StringReverseWord {
+    public static void main(String[] args) {
+
+        String str = "DLROW OLLEH", rstr="";
+        char ch;
+
+        for(int i=0; i<str.length(); i++){
+
+            ch=str.charAt(i);
+            rstr=ch+rstr;
+        }
+        System.out.println(rstr);
+    }
+}
