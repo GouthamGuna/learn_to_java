69bedc5e27001b2c7e57dd2d9da1f1ab4e180e66
gouthamguna <ggowthamsankar@gmail.com>
2022-12-13 15:05:14 +0530

    inital commit

---
+++ office_system/suresh_bro_system/practice_java/test/src/string/StringReverseWord.java
@@ -0,0 +1,18 @@
+package string;
+
+public class StringReverseWord{
+    public static void main(String[] args) {
+
+        String str = "DLROW OLLEH", rstr = "";
+        char ch;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            ch = str.charAt(i);
+            rstr = ch + rstr;
+
+            System.out.print(i);
+        }
+        System.out.println(" " + rstr);
+    }
+}
