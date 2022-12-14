9f8e8c4e6e4f4f48f74a96e49bfbf575fd6a9811
gouthamguna <ggowthamsankar@gmail.com>
2022-12-14 10:11:09 +0530

    initial commit

---
+++ office_system/suresh_bro_system/learn_spring_framework/src/main/java/in/gmsk/springbootapp/LearnSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package in.gmsk.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+	}
+
+}
