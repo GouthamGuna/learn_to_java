b6eb0a8ed7ef7fea1143fb27af2743e195c81d04
GouthamGuna <ggowthamsankar@gmail.com>
2022-12-17 17:03:22 +0530

    python

---
+++ asus_system/python3/ransomeware.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+import os
+
+# let's find some files
+
+files = []
+
+for file in os.listdir();
+        if file == "learnpython.py";
+                continue
+        file.append(file)
+
+print(files)
