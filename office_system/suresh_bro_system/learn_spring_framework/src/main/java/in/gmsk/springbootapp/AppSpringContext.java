3f00ae2af13fd07f45d4045952effbde3d7abdab
GouthamGuna <ggowthamsankar@gmail.com>
2022-12-21 15:27:00 +0530

    learn at @Bean customized name declare

--- office_system/suresh_bro_system/learn_spring_framework/src/main/java/in/gmsk/springbootapp/AppSpringContext.java
+++ office_system/suresh_bro_system/learn_spring_framework/src/main/java/in/gmsk/springbootapp/AppSpringContext.java
@@ -5,10 +5,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppSpringContext {
     public static void main(String[] args) {
 
-        // Launch the Spring Context
+        // 1. Launch the Spring Context
         var context=new AnnotationConfigApplicationContext(BeanConfiguration.class);
 
-        // Retrieve Bean manged by Spring
-        System.out.println(context.getBean("getExample"));
+        // 2. Configure the things that we want spring to manage the BeanConfiguration -@Configuration & @Bean
+
+        // 3. Retrieve Bean manged by Spring
+        System.out.println(context.getBean("getName"));
+
+        System.out.println(context.getBean("getAge"));
+
+        System.out.println(context.getBean("person"));
+
+        System.out.println(context.getBean("address2"));
     }
 }
