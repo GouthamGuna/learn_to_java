3988b1c52981ed306c932050dbc3e99fd514e811
gouthamguna <ggowthamsankar@gmail.com>
2023-01-02 12:39:42 +0530

    learn python datatypes

---
+++ office_system/suresh_bro_system/python3.6.4/datatypes.py
@@ -0,0 +1,46 @@
+
+fstring = "Hello World!"
+
+print ("--------------------------------------------------------")
+print(f"This is my first program in python : {fstring}")
+print(f"Learn About Python Datatypes")
+print ("--------------------------------------------------------")
+
+# python DataTypes 
+# int, long, float, complex = holds complex numbers, string
+
+#int
+
+x,y=10,20
+print (f"Total = {x+y} ")
+
+print ("--------------------------------------------------------")
+
+# long
+
+exmobileno = 9876543210
+print(f"This my crush mobile number :{exmobileno} :-)")
+
+print ("--------------------------------------------------------")
+
+#float
+
+area = 10.05
+hgt = 15.06
+print(f"size of {area} & {hgt}")
+
+print ("--------------------------------------------------------")
+
+#string 
+
+crushname = "gmsk"
+print(f"I love my self {crushname}")
+
+print ("--------------------------------------------------------")
+
+#complex  
+
+accountno = 98765432109876543210
+print(f"This my DBS Bank Account No : {accountno}")
+
+print ("--------------------------------------------------------")
