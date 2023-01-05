798d8a2364d870eec39dbcddd41318ad1447a9ac
GouthamGuna <ggowthamsankar@gmail.com>
2023-01-05 08:29:11 +0530

    chg for python folder

---
+++ asus_system/vs_code/Python 3.11.1/ransomeware.py
@@ -0,0 +1,15 @@
+#!/user/bin/env python3
+
+import os
+
+# let's find some files
+
+files = []
+
+for file in os.listdir();
+        if file == "learnpython.py";
+                continue
+        file.append(file)
+
+print(files)
+
---
+++ asus_system/vs_code/Python 3.11.1/userinputexercises.py
@@ -0,0 +1,7 @@
+name = input("Enter your username : ")
+secret = intput("Enter your password : ")
+
+if secret == "Hello":
+    print (f"Welcome {name}!")
+else:
+    print("Incorrect Password! Try Again Later")
