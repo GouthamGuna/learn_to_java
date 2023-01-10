5b9e29075651be5c15f2ced39f4026175b6771fa
GouthamGuna <ggowthamsankar@gmail.com>
2023-01-10 18:57:58 +0530

    learn string method

--- asus_system/learn_java/src/test/StudentRankCalculation.java
+++ asus_system/learn_java/src/test/StudentRankCalculation.java
@@ -4,15 +4,15 @@ public class StudentRankCalculation {
 
     /**
      *  100 - 90 = A
-     *  90 - 80 = B
-     *  80 - 70 = c
-     *  70 - 60 = D
-     *  60 - 50 = E
-     *       50 < F
+     *  90 - 80  = B
+     *  80 - 70  = c
+     *  70 - 60  = D
+     *  60 - 50  = E
+     *       50  < F
      */
 
-    public String getGrade(int max, int min){
+    public static void main(String[] args) {
 
-        return "";
+
     }
 }
