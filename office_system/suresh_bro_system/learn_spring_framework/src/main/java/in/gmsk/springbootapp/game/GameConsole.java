d9d30671abc6500f8bc4b6b55920ef914cf4edb2
gouthamguna <ggowthamsankar@gmail.com>
2023-01-18 13:22:14 +0530

    delete

--- office_system/suresh_bro_system/learn_spring_framework/src/main/java/in/gmsk/springbootapp/game/GameConsole.java
+++
@@ -1,9 +0,0 @@
-package in.gmsk.springbootapp.game;
-
-public interface GameConsole {
-
-    void up();
-    void down();
-    void left();
-    void right();
-}
--- office_system/suresh_bro_system/python3.6.4/if_else_stmt.py
+++
@@ -1,9 +0,0 @@
-x = 24
-y = 27
-
-if(x > y):
-    print(f"{x} is Less then {y}")
-elif(x == y):
-    print(f"{x} is {y} are equal")
-else:
-    print(f"{y} is Greater then {x}")
