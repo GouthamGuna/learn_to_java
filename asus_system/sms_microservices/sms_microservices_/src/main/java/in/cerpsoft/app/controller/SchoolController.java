c44cc064c6d8180c9a2c9fe06657c6ae9e5f06da
GouthamGuna <ggowthamsankar@gmail.com>
2023-03-07 16:32:33 +0530

    school controller chg

--- asus_system/sms_microservices/sms_microservices_/src/main/java/in/cerpsoft/app/controller/SchoolController.java
+++ asus_system/sms_microservices/sms_microservices_/src/main/java/in/cerpsoft/app/controller/SchoolController.java
@@ -5,10 +5,9 @@ import in.cerpsoft.app.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/school/api")
@@ -22,5 +21,16 @@ public class SchoolController {
     @PostMapping("/save")
     public ResponseEntity<SchoolDto> saveSchool(@RequestBody SchoolDto schoolDto){
         return new ResponseEntity<>(schoolService.saveSchoolDetails(schoolDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/schooldetails")
+    public List<SchoolDto> findByAll(SchoolDto schoolDto){
+        return schoolService.schoolDetailsList();
+    }
+
+    @GetMapping("/schooldetails/edit/{id}")
+    public ResponseEntity<SchoolDto> findById(@PathVariable("id") int id,
+                                              @RequestBody SchoolDto schoolDto){
+        return new ResponseEntity<>(schoolService.getSchoolById(id), HttpStatus.OK);
     }
 }
