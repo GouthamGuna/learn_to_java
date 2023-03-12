cda514533124aff088756f8abee0046cf4ca0f21
GouthamGuna <ggowthamsankar@gmail.com>
2023-03-12 13:44:43 +0530

    validate the input

--- asus_system/sms_microservices/sms_microservices_/src/main/java/in/cerpsoft/app/controller/SchoolController.java
+++ asus_system/sms_microservices/sms_microservices_/src/main/java/in/cerpsoft/app/controller/SchoolController.java
@@ -2,6 +2,7 @@ package in.cerpsoft.app.controller;
 
 import in.cerpsoft.app.dto.SchoolDto;
 import in.cerpsoft.app.service.SchoolService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +20,28 @@ public class SchoolController {
         this.schoolService = schoolService;
     }
     @PostMapping("/save")
-    public ResponseEntity<SchoolDto> saveSchool(@RequestBody SchoolDto schoolDto){
+    public ResponseEntity<SchoolDto> saveSchool(@RequestBody @Valid SchoolDto schoolDto){
         return new ResponseEntity<>(schoolService.saveSchoolDetails(schoolDto), HttpStatus.CREATED);
     }
 
-    @GetMapping("/schooldetails")
-    public List<SchoolDto> findByAll(SchoolDto schoolDto){
-        return schoolService.schoolDetailsList();
+    @GetMapping("/fetchAll")
+    public ResponseEntity<List<SchoolDto>> findByAll(SchoolDto schoolDto){
+        return ResponseEntity.ok(schoolService.schoolDetailsList());
     }
 
-    @GetMapping("/schooldetails/edit/{id}")
+    @GetMapping("/findById/{id}")
     public ResponseEntity<SchoolDto> findById(@PathVariable("id") int id,
                                               @RequestBody SchoolDto schoolDto){
         return new ResponseEntity<>(schoolService.getSchoolById(id), HttpStatus.OK);
     }
 
-    @PutMapping("/schooldetails/update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<SchoolDto> upDateSchoolDetails(@RequestBody SchoolDto schoolDto,
                                                          @PathVariable("id") int id){
         return new ResponseEntity<>(schoolService.upDateSchoolDetails(schoolDto, id), HttpStatus.OK);
     }
 
-    @DeleteMapping("/schooldetails/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteSchoolData(@PathVariable("id") int id){
         schoolService.deleteSchoolDetails(id);
         return new ResponseEntity<>("School Details Deleted Successfully...", HttpStatus.OK);
