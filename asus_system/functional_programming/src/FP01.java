57afb9d4177bc78de51e8415440e88a655c2fe1a
GouthamGuna <ggowthamsankar@gmail.com>
2023-03-31 22:30:46 +0530

    exercises 01

--- asus_system/functional_programming/src/FP01.java
+++ asus_system/functional_programming/src/FP01.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.*;
+
 public class FP01 {
     public static void main(String[] args) {
+
+        List<Integer> newList = Arrays.asList(0,1,2,3,4,5,6,7,8,10);
+
+        newList.stream()                                   // stream is a sequence of element
+                .filter(evenNumber -> evenNumber % 2 == 0) // lambda expression
+                .forEach(out::println);                     // method reference
 
     }
 }
---
+++ asus_system/functional_programming/src/FP01_Exercises.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.System.out;
+
+public class FP01_Exercises {
+    public static void main(String[] args) {
+
+        List<Integer> newList = Arrays.asList(0,1,2,3,4,5,6,7,8,10);
+        printOddNumbers(newList);
+
+        List<String>  courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "Docker");
+        printCourses(courses);
+        printFourLetters(courses);
+    }
+
+    private static void printFourLetters(List< String> courses) {
+        courses.stream()
+                .filter(course -> course.length() >= 4)
+                .forEach(out::println);
+    }
+
+    private static void printCourses(List< String> courses) {
+
+        courses.stream()
+                .filter(coursesContains -> coursesContains.contains("Spring"))
+                .forEach(out::println);
+    }
+
+    private static void printOddNumbers(List< Integer> newList) {
+
+        newList.stream()                                    // stream is a sequence of element
+                .filter(oddNumber -> oddNumber % 2 != 0)    // lambda expression
+                .forEach(out::println);                     // method reference
+    }
+}
