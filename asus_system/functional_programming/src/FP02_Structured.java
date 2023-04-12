ae5f099b4964085850912eab4c130c3d91b92ae0
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-12 13:22:41 +0530

    FP02_Structured.java

--- asus_system/functional_programming/src/FP02_Structured.java
+++ asus_system/functional_programming/src/FP02_Structured.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FP02_Structured {
+import java.util.List;
+
+import static java.lang.System.*;
+
+public class FP02_Structured {
+    public static void main(String[] args) {
+        
+        List<Integer> numbers = List.of(12,9,45,24,27,1,2,3,4,5);
+        addListOfNumbers(numbers);
+    }
+
+    private static void addListOfNumbers(List<Integer> numbers) {
+        int sum = 0;
+        
+        for(int i: numbers){
+            sum += i;
+        }
+        out.println(sum);
+    }
 }
