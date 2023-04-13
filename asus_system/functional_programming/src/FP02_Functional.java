d111920af07f7d3ea301b30f20f127b72da5e888
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-13 10:46:33 +0530

    Update FP02_Functional.java

--- asus_system/functional_programming/src/FP02_Functional.java
+++ asus_system/functional_programming/src/FP02_Functional.java
@@ -6,12 +6,19 @@ public class FP02_Functional {
     public static void main(String[] args) {
         
         List<Integer> numbers = List.of(12,9,45,24,27,1,2,3,4,5);
-        addListOfNumbers(numbers);
+        int output = addListOfNumbers(numbers);
+        out.println(output);
     }
 
-    private static void addListOfNumbers(List<Integer> numbers) {
-        numbers.stream()
-                .map(number -> number + number)
-                .forEach(out::println);
+    private static int addListOfNumbers(List<Integer> numbers) {
+       return numbers.stream()
+               // .reduce(0, FP02_Functional::sumOfTheNumbers);   //custom function
+               //.reduce(0,(x,y) -> x+y);                         //lambda expression
+               .reduce(0, Integer::sum);                    //method reference
+
+    }
+
+    private static Integer sumOfTheNumbers(Integer Aggregate, Integer nextNumber) {
+        return Aggregate + nextNumber;
     }
 }
