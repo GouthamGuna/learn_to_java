8c416a45bd5095fdd6d08da4c0fd9c0348ef308a
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-21 15:47:57 +0530

    Update StudentSubjectMarksMappingTest.java

--- asus_system/marksheet_report/test/helper/StudentSubjectMarksMappingTest.java
+++ asus_system/marksheet_report/test/helper/StudentSubjectMarksMappingTest.java
@@ -1,4 +1,14 @@
+package helper;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StudentSubjectMarksMappingTest {
-  
+
+    @Test
+    void setStudentSubjectMarksMapping() {
+        StudentSubjectMarksMapping mapping = new StudentSubjectMarksMapping();
+        mapping.setStudentSubjectMarksMapping();
+    }
 }
