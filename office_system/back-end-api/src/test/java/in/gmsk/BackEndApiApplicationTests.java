8c0363111a667924d9efb9157da85006b9ab9fb5
GouthamGuna <ggowthamsankar@gmail.com>
2023-05-03 12:38:47 +0530

    initial commit

---
+++ office_system/back-end-api/src/test/java/in/gmsk/BackEndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.gmsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
