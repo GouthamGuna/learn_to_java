5b1f27fcf179d36fbd043c221420b3b38e5f155c
GouthamGuna <ggowthamsankar@gmail.com>
2023-05-16 15:40:34 +0530

    initial commit

--- office_system/back-end-api/src/main/java/in/gmsk/entity/ParentEntity.java
+++ office_system/back-end-api/src/main/java/in/gmsk/entity/ParentEntity.java
@@ -1,2 +1,18 @@
-package in.gmsk.entity;public class ParentEntity {
+package in.gmsk.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "parent")
+public class ParentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String studentId;
+    private String name;
+    private String mobileNo;
+    private String mailId;
 }
