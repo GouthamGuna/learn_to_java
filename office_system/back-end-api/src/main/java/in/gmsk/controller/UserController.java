cb59cec81c7c2769300cc2cf8906478cd3c3b94e
GouthamGuna <ggowthamsankar@gmail.com>
2023-05-16 15:40:05 +0530

    Update UserController.java

--- office_system/back-end-api/src/main/java/in/gmsk/controller/UserController.java
+++ office_system/back-end-api/src/main/java/in/gmsk/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class UserController {
 
     private final Logger logger =
