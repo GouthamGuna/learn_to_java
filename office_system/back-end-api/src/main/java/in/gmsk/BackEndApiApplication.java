656b2aac095ab295018b040c8d3e4b879197be89
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-02 19:18:25 +0530

    Update main class

--- office_system/back-end-api/src/main/java/in/gmsk/BackEndApiApplication.java
+++ office_system/back-end-api/src/main/java/in/gmsk/BackEndApiApplication.java
@@ -19,7 +19,7 @@ public class BackEndApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5500")
+						.allowedOrigins("http://localhost:4200")
 						.allowedMethods("POST","GET","PUT","DELETE");
 			}
 		};
