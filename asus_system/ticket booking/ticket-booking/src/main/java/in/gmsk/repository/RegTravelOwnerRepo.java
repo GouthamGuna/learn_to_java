9e129901ac1992406835faaa80fada10efacf47a
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-03 07:44:46 +0530

    remove ticket_booking app

--- asus_system/ticket booking/ticket-booking/src/main/java/in/gmsk/repository/RegTravelOwnerRepo.java
+++
@@ -1,9 +0,0 @@
-package in.gmsk.repository;
-
-import in.gmsk.model.RegistrationTravelOwner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegTravelOwnerRepo extends JpaRepository<RegistrationTravelOwner, Integer> {
-}
--- asus_system/ticket booking/ticket-booking/src/main/java/in/gmsk/service/RegTravelOwnerService.java
+++
@@ -1,8 +0,0 @@
-package in.gmsk.service;
-
-import in.gmsk.model.RegistrationTravelOwner;
-
-public interface RegTravelOwnerService {
-
-    RegistrationTravelOwner saveTravelsOwnerDetails(RegistrationTravelOwner travelOwner);
-}
