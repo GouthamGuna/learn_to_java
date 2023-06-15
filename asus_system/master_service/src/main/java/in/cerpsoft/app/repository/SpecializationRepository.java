c3a2b5305034768216e88076f45aa3ed0f1f10c4
GouthamGuna <ggowthamsankar@gmail.com>
2023-06-16 00:59:43 +0530

    remove master_service from gmsk

--- asus_system/master_service/src/main/java/in/cerpsoft/app/repository/SpecializationRepository.java
+++
@@ -1,9 +0,0 @@
-package in.cerpsoft.app.repository;
-
-import in.cerpsoft.app.model.SpecializationDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpecializationRepository extends JpaRepository< SpecializationDto, Integer > {
-}
