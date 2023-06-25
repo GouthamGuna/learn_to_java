4b0a172a6960690cfee7c44ad6242df4dca20712
Goutham <ggowthamsankar@gmail.com>
2023-06-25 18:25:55 +0530

    Update ExampleStringBuffer.java

--- asus_system/learn_java/src/string/ExampleStringBuffer.java
+++ asus_system/learn_java/src/string/ExampleStringBuffer.java
@@ -1,2 +1,15 @@
-package string;public class ExampleStringBuffer {
+package string;
+
+public class ExampleStringBuffer {
+
+    public static void main(String[] args) {
+
+        String subjectName = "Hello";
+
+        StringBuilder stringBuilder =new StringBuilder();
+        stringBuilder.append("Hai" + ", ").append(subjectName);
+
+        System.out.println("stringBuilder() : "+stringBuilder);
+
+    }
 }
