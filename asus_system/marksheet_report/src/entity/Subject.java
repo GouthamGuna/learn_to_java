b6f65edb1b250399085d57b72662d373b8344e5f
Goutham Guna <ggowthamsankar@gmail.com>
2023-06-25 08:52:38 +0530

    Update Subject.java

--- asus_system/marksheet_report/src/entity/Subject.java
+++ asus_system/marksheet_report/src/entity/Subject.java
@@ -1,2 +1,28 @@
-package entity;public class Subject {
+package entity;
+
+public class Subject {
+
+    private String subjectName;
+    private int subjectMark;
+
+    public Subject(String subjectName, int subjectMark) {
+        this.subjectName = subjectName;
+        this.subjectMark = subjectMark;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getSubjectMark() {
+        return subjectMark;
+    }
+
+    public void setSubjectMark(int subjectMark) {
+        this.subjectMark = subjectMark;
+    }
 }
