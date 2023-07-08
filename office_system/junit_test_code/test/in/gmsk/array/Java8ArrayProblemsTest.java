8e7a276f7e9af3f60b599af2794e31a4ca4ce2de
Goutham <ggowthamsankar@gmail.com>
2023-07-08 17:41:17 +0530

    Update Java8ArrayProblemsTest.java

--- office_system/junit_test_code/test/in/gmsk/array/Java8ArrayProblemsTest.java
+++ office_system/junit_test_code/test/in/gmsk/array/Java8ArrayProblemsTest.java
@@ -1,16 +1,26 @@
 package in.gmsk.array;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Java8ArrayProblemsTest {
 
 	@Test
 	void testFetchSecondSmallNumber() {
+		List<Integer> output = null;
 		
-		int[] args = {5,2,8,3,1};
+		//int[] arr = {5,2,8,3,1};
+		
+		int[] arr1 = {1,2,3,4,5};
+		int[] arr2 = {4,5,8,9,7,1};
 		
 		Java8ArrayProblems instance = new Java8ArrayProblems();
-		int output = instance.fetchSecondSmallNumber(args);
+		//int output = instance.fetchSecondSmallNumber(arr);
+		
+		//System.out.println("output : "+output);
+		
+		output = instance.fetchingSameElementInArrays(arr1, arr2);
 		
 		System.out.println("output : "+output);
 	}
