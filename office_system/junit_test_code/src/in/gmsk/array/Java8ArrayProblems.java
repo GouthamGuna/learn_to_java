a30a056535977101b10a9d8e453dc264446d906d
Goutham <ggowthamsankar@gmail.com>
2023-07-08 17:40:33 +0530

    Update Java8ArrayProblems.java

--- office_system/junit_test_code/src/in/gmsk/array/Java8ArrayProblems.java
+++ office_system/junit_test_code/src/in/gmsk/array/Java8ArrayProblems.java
@@ -1,11 +1,24 @@
 package in.gmsk.array;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Java8ArrayProblems {
 
-	public int fetchSecondSmallNumber(int[] args) {
-		return Arrays.stream(args).distinct().sorted().skip(1).findFirst()
-				.orElseThrow(() -> new IllegalArgumentException("There is not have number."));
+	public int fetchSecondSmallNumber(int[] arr) {
+		
+		return Arrays.stream(arr).distinct().sorted().skip(1).findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("There is not a second number."));
+	}
+	
+	public List<Integer> fetchingSameElementInArrays(int[] arr1, int[] arr2) {
+		
+		List<Integer> output = Arrays.stream(arr1).filter(number -> Arrays.stream(arr2)
+				.anyMatch(arr2Number -> arr2Number == number))
+				.boxed()
+				.collect(Collectors.toList());
+		
+		return output;
 	}
 }
