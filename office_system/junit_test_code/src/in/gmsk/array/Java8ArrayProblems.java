ce02d4572bff8b6ae3f3d5544212eda9105c38d8
Goutham <ggowthamsankar@gmail.com>
2023-07-10 17:25:30 +0530

    Update Java8ArrayProblems.java

--- office_system/junit_test_code/src/in/gmsk/array/Java8ArrayProblems.java
+++ office_system/junit_test_code/src/in/gmsk/array/Java8ArrayProblems.java
@@ -1,8 +1,14 @@
 package in.gmsk.array;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.lang.System.*;
 
 public class Java8ArrayProblems {
 
@@ -20,5 +26,38 @@ public class Java8ArrayProblems {
 				.collect(Collectors.toList());
 
 		return output;
+	}
+	
+	public void diffBetweenMonthName() {
+		
+		LocalDate date1 = LocalDate.of(2023, 5, 8);
+		LocalDate date2 = LocalDate.of(2023, 4, 1);
+		
+		Period diff = Period.between(date1, date2);
+		System.out.println(diff); //3
+		
+	}
+	
+	public void monthNames() {
+		
+		LocalDate date1 = LocalDate.of(2023, 4, 1);
+		LocalDate date2 = LocalDate.of(2023, 5, 8);
+		
+		String outPut;
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM");
+		List<String> months = new ArrayList<>();
+
+		while (!date1.isAfter(date2)) {
+		    String monthName = date1.format(formatter);
+		    months.add(monthName);
+		    date1 = date1.plusMonths(1);
+		}
+
+		System.out.println("Months between dates: " + months);
+		
+		
+		months.stream()
+		.forEach(out::println);
 	}
 }
