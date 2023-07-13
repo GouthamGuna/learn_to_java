7c28f5e9a6afe14589999055c54b7aba2bfa371e
Goutham <ggowthamsankar@gmail.com>
2023-07-13 05:45:38 +0530

    Update SICI.java

--- asus_system/learn_java/src/test/SICI.java
+++ asus_system/learn_java/src/test/SICI.java
@@ -1,2 +1,17 @@
-package test;public class SICI {
+package test;
+
+public class SICI {
+
+    public static void calculation(double pr, double t, double rate){
+
+        double sim = (pr * t * rate)/100;
+        double com = pr * Math.pow(1.0+rate/100.0,t) - pr;
+
+        System.out.println("Simple Interest="+sim);
+        System.out. println("Compound Interest="+com);
+    }
+
+    public static void main(String[] args) {
+        calculation(11, 8, 1000);
+    }
 }
