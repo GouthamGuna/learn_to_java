69809b1004c16f699d0b3fa4f22692a1a0fac09e
Goutham <ggowthamsankar@gmail.com>
2023-07-13 05:46:13 +0530

    Update Reverse.java

--- asus_system/learn_java/src/string/Reverse.java
+++ asus_system/learn_java/src/string/Reverse.java
@@ -1,2 +1,16 @@
-package string;public class Reverse {
+package string;
+
+public class Reverse {
+
+    public static String reverse(String reverseSentence){
+
+        if(reverseSentence.isBlank())
+            return reverseSentence;
+
+        return reverse(reverseSentence.substring(1)) + reverseSentence.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("Gowtham Sankar"));
+    }
 }
