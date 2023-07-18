a433e25146dbdd49af8c1fee878afff1b4dc2dfe
Goutham <ggowthamsankar@gmail.com>
2023-07-18 08:34:26 +0530

    Update EuclidAlgorithm.java

--- asus_system/learn_java/src/robert_algorithums/EuclidAlgorithm.java
+++ asus_system/learn_java/src/robert_algorithums/EuclidAlgorithm.java
@@ -1,2 +1,36 @@
-package robert_algorithums;public class EuclidAlgorithm {
+package robert_algorithums;
+
+public class EuclidAlgorithm {
+
+    public static int gcd(int u, int v){
+        int t;
+        if (u > 0) {
+            do {
+                if (u < v) {
+                    t = u;
+                    u = v;
+                    v = t;
+                    u = u - v;
+                }
+                break;
+            } while (u > 0);
+        }
+        return v;
+    }
+
+    public static int mainStream(int x, int y){
+
+        /*while (cin >> x && cin >> y){
+            if(x > 0 && y > 0)
+                count << x << ' ' << y << ' ' << gcd(x, y) << '\n';
+        }*/
+
+
+
+       return gcd(x, y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("output : "+mainStream(20, 10));
+    }
 }
