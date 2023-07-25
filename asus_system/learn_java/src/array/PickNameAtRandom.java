51dfdc70f7a1aee06bf021ef4a056accc53ab5a6
Goutham <ggowthamsankar@gmail.com>
2023-07-25 09:25:52 +0530

    Update PickNameAtRandom.java

--- asus_system/learn_java/src/array/PickNameAtRandom.java
+++ asus_system/learn_java/src/array/PickNameAtRandom.java
@@ -1,2 +1,27 @@
-package array;public class PickNameAtRandom {
+package array;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class PickNameAtRandom {
+
+    public static String getRandomName(List<String> listOfString) {
+        String randomString = null;
+
+        if (0 != listOfString.size()) {
+            var rand = new Random();
+            int index = rand.nextInt(listOfString.size());
+            randomString = listOfString.get(index);
+        }
+
+        return randomString;
+    }
+
+    public static void main(String[] args) {
+
+        List<String> nameList = Arrays.asList("Kavi", "Visi", "GMSK");
+
+        System.out.println("Your random name is " + getRandomName(nameList) + " now!");
+    }
 }
