077320ee22560f2a1c7ac96c4d3d48e8661b80a3
Goutham <ggowthamsankar@gmail.com>
2023-07-25 09:26:23 +0530

    Update TextFileReader.java

--- asus_system/learn_java/src/test/TextFileReader.java
+++ asus_system/learn_java/src/test/TextFileReader.java
@@ -1,2 +1,13 @@
-package test;public class TextFileReader {
+package test;
+
+import java.io.IOException;
+
+public class TextFileReader {
+
+    public static void main(String[] args) throws IOException {
+
+        /*Path readme = Path.of("/home/gowtham/Documents/work/notes.md");
+        Files.lines(readme, StandardCharsets.UTF_8)
+                .forEach(System.out::println);*/
+    }
 }
