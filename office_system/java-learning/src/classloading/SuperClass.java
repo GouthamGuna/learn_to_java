d8a90fbf19d430cedbb5af635520c4fbfdfb1f5a
Goutham <ggowthamsankar@gmail.com>
2023-08-21 17:41:38 +0530

    Update IDE.xml

--- office_system/java-learning/src/classloading/SuperClass.java
+++ office_system/java-learning/src/classloading/SuperClass.java
@@ -4,6 +4,7 @@ public class SuperClass {
 
     static {
         System.out.println("Super class init...");
+        System.out.println("Sub class value = " +SubClass.value);
     }
 
     public static int value = 123;
