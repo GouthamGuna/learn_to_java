9076e217d37653ec2ff6ca3975435444098767b8
Goutham <ggowthamsankar@gmail.com>
2023-08-24 23:22:19 +0530

    Update PassArray.java

--- asus_system/learn_java/src/array/PassArray.java
+++ asus_system/learn_java/src/array/PassArray.java
@@ -1,2 +1,27 @@
-package array;public class PassArray {
+package array;
+
+public class PassArray {
+
+    static void vaTest(int v[]){
+
+        System.out.print("Number of args : "+ v.length + " Contents : ");
+
+        for(int x : v){
+            System.out.print(x + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // How an array must be create to hold the arguments
+
+        int[] n1 = {10};
+        int[] n2 = {1, 2, 3};
+        int[] n3 = {};
+
+        vaTest(n1);
+        vaTest(n2);
+        vaTest(n3);
+    }
 }
