cb657b55f9b29fac19738beae2bcc563c4f001e7
Goutham <ggowthamsankar@gmail.com>
2023-08-29 17:25:07 +0530

    Update JustThinkingProgramming.class

--- office_system/java-learning/src/test/JustThinkingProgramming.java
+++ office_system/java-learning/src/test/JustThinkingProgramming.java
@@ -3,10 +3,10 @@ package test;
 public class JustThinkingProgramming {
 
     public static void main(String[] args) {
-
+        someWork();
     }
 
-    public synchronized void someWork() {
+    private synchronized static void someWork() {
 
         int i = 20;
         System.out.println(i);
