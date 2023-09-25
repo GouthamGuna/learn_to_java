34ec8e4e0e65c31f606ba89a08c293901a59ed50
Goutham <ggowthamsankar@gmail.com>
2023-09-25 14:45:46 +0530

    Update GenerateUserNameTest.java

--- office_system/image_upload/test/in/gmsk/util/GenerateUserNameTest.java
+++ office_system/image_upload/test/in/gmsk/util/GenerateUserNameTest.java
@@ -1,5 +1,12 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.gmsk.util;
+
+import org.junit.jupiter.api.Test;
 
 class GenerateUserNameTest {
 
+    @Test
+    void getName() {
+        String output = GenerateUserName.getName();
+        System.out.println("output = " + output);
+    }
 }
