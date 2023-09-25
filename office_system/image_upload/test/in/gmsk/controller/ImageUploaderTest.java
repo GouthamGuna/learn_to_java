a97432352e2a78bc90dbcaf447a74e6ee0513b87
Goutham <ggowthamsankar@gmail.com>
2023-09-25 14:44:35 +0530

    Update ImageUploaderTest.java

--- office_system/image_upload/test/in/gmsk/controller/ImageUploaderTest.java
+++ office_system/image_upload/test/in/gmsk/controller/ImageUploaderTest.java
@@ -1,5 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.gmsk.controller;
+
+import org.junit.jupiter.api.Test;
 
 class ImageUploaderTest {
+    private ImageUploader uploader = new ImageUploader();
 
+    @Test
+    void uploadImageTest() {
+        String imagePath, output;
+
+        imagePath = "C:\\Users\\User\\Downloads\\davy-jones-pirates-of-the-caribbean.jpg";
+        output = uploader.uploadImage(imagePath);
+
+        System.out.println("output = " + output);
+    }
+
+    @Test
+    void downloadImageTest() {
+        String downloadPath, output;
+
+        downloadPath = "C:\\Users\\User\\Downloads\\Output_Stream_Img.jpg";
+        output = uploader.downloadImage(downloadPath);
+
+        System.out.println("output = " + output);
+    }
 }
