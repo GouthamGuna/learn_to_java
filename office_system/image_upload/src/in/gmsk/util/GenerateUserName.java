a83aff0faf213ea341966a758f69cb27d8869a3d
Goutham <ggowthamsankar@gmail.com>
2023-09-25 14:44:15 +0530

    Update GenerateUserName.java

--- office_system/image_upload/src/in/gmsk/util/GenerateUserName.java
+++ office_system/image_upload/src/in/gmsk/util/GenerateUserName.java
@@ -1,4 +1,20 @@
 package in.gmsk.util;
 
+import java.util.Random;
+
 public class GenerateUserName {
+
+    public static String getName() {
+
+        final String ALPHA_NUMERIC_STRING = "abcdefghijklmnopqrstuvwxyz";
+
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int index = random.nextInt(ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(index));
+        }
+        return builder.toString();
+    }
 }
