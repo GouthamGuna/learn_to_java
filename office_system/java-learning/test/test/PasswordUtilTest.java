7106b7d60e4a129f93b67ede093b0375090cfbbf
Goutham <ggowthamsankar@gmail.com>
2023-09-27 17:19:39 +0530

    Update PasswordUtilTest.java

--- office_system/java-learning/test/test/PasswordUtilTest.java
+++ office_system/java-learning/test/test/PasswordUtilTest.java
@@ -1,5 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package test;
+
+import org.junit.jupiter.api.Test;
 
 class PasswordUtilTest {
 
+    @Test
+    void encodePassword() {
+        String output, password = "gMSK@3210";
+        output = PasswordUtil.encodePassword(password);
+
+        System.out.println("output = " + output);
+    }
 }
