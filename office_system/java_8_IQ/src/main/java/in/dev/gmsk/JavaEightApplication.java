a9a9ae906fb37563b13a8b082ee9ff844f03ea7e
Goutham <ggowthamsankar@gmail.com>
2023-10-21 14:53:01 +0530

    Update JavaEightApplication.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
@@ -1,8 +1,21 @@
 package in.dev.gmsk;
 
+import in.dev.gmsk.abstraction.MyClass;
+import in.dev.gmsk.abstraction.MyInterface;
+
 public class JavaEightApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello Lunar *)");
+
+        MyInterface object = new MyClass();
+        object.methodA();
+
+        System.out.println("methodB() return int = "+object.methodB());
+
+        String output = object.methodC(10.50, 20.50);
+        System.out.println("output = " + output);
+
+        System.out.println(object.methodD());
     }
 }
