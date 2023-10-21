729a4850bf626e3cb43827ebffd3eb95cd494ab8
Goutham <ggowthamsankar@gmail.com>
2023-10-21 10:21:53 +0530

    update files

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
@@ -1,4 +1,8 @@
 package in.dev.gmsk;
 
 public class JavaEightApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Lunar *)");
+    }
 }
--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/JavaEightApplicationTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/JavaEightApplicationTest.java
@@ -1,5 +1,13 @@
+package in.dev.gmsk;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaEightApplicationTest {
 
+    @Test
+    void mainTest() {
+        assertTrue(true);
+    }
 }
