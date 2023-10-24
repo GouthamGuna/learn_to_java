b7f4f6ed14929f568694e1326ac31c0b34a4c2dc
Goutham <ggowthamsankar@gmail.com>
2023-10-24 11:47:01 +0530

    Update DeserializationMainMethod.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/keywords/transient_kw/DeserializationMainMethod.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/keywords/transient_kw/DeserializationMainMethod.java
@@ -1,4 +1,25 @@
 package in.dev.gmsk.keywords.transient_kw;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class DeserializationMainMethod {
+
+    public static void main(String[] args) {
+
+        try{
+
+            ObjectInputStream inputStream =
+                    new ObjectInputStream(new FileInputStream("C:\\Users\\User\\Desktop\\log\\UserLoginAccess.txt"));
+
+            UserLoginAccess userLoginAccess = (UserLoginAccess) inputStream.readObject();
+
+            System.out.println("UserName : "+userLoginAccess.username+" Password : "+userLoginAccess.password);
+            inputStream.close();
+
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
