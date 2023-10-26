a1442a9b17b2487c8496ac94c4d0310f28920e16
Goutham <ggowthamsankar@gmail.com>
2023-10-26 15:29:13 +0530

    Update AnnotatedSubClass, AnnotatedSuperClass.java

--- office_system/creating_annotations/src/main/java/in/dev/gmsk/controller/AnnotatedSubClass.java
+++ office_system/creating_annotations/src/main/java/in/dev/gmsk/controller/AnnotatedSubClass.java
@@ -1,4 +1,11 @@
 package in.dev.gmsk.controller;
 
-public class AnnotatedSubClass {
+import in.dev.gmsk.annotations.InheritedAnnotation;
+
+public class AnnotatedSubClass extends AnnotatedSuperClass{
+
+    @Override
+    public void methodOne() {
+        System.out.println("AnnotatedSubClass is true : " + AnnotatedSubClass.class.isAnnotationPresent(InheritedAnnotation.class));
+    }
 }
--- office_system/creating_annotations/src/main/java/in/dev/gmsk/controller/AnnotatedSuperClass.java
+++ office_system/creating_annotations/src/main/java/in/dev/gmsk/controller/AnnotatedSuperClass.java
@@ -1,4 +1,17 @@
 package in.dev.gmsk.controller;
 
+import in.dev.gmsk.annotations.InheritedAnnotation;
+
+@InheritedAnnotation
 public class AnnotatedSuperClass {
+
+    public void methodOne(){
+        System.out.println("AnnotatedSuperClass is true : " + AnnotatedSuperClass.class.isAnnotationPresent(InheritedAnnotation.class));
+    }
+
+    public static void main(String[] args) {
+
+        AnnotatedSuperClass annotatedSuperClass = new AnnotatedSubClass();
+        annotatedSuperClass.methodOne();
+    }
 }
