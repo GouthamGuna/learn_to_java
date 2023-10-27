8c42e93dd2d3edb9cddefc5908ab0a0e11031496
Goutham <ggowthamsankar@gmail.com>
2023-10-27 19:23:17 +0530

    Update JDBCConnection.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/jdbc/JDBCConnection.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/jdbc/JDBCConnection.java
@@ -1,4 +1,16 @@
 package in.dev.gmsk.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConnection {
+
+    private static final String url = "jdbc:mysql://localhost:3306/pearl";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    public static Connection getDataBaseConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
