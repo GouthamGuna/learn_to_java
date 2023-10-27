265afb4d34da073865745ce4f7e4ff96820dbbe3
Goutham <ggowthamsankar@gmail.com>
2023-10-27 19:22:39 +0530

    Update JDBC_ENUM.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/enum_pkg/JDBC_ENUM.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/enum_pkg/JDBC_ENUM.java
@@ -1,4 +1,22 @@
 package in.dev.gmsk.enum_pkg;
 
 public enum JDBC_ENUM {
+
+    driver("com.mysql.jdbc.Driver"),
+    protocol("jdbc.mysql"),
+    host("localhost"),
+    port("3306"),
+    database("krithvik"),
+    username("root"),
+    password("root");
+
+    private String values;
+
+    public String getValues(){
+        return this.values;
+    }
+
+    JDBC_ENUM(String values){
+        this.values = values;
+    }
 }
