8d790dbae8d57a549594af90db5026b078a93dd9
Goutham <ggowthamsankar@gmail.com>
2023-10-30 18:15:31 +0530

    Update Employee.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/Employee.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/Employee.java
@@ -1,4 +1,19 @@
 package in.dev.gmsk.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Setter
+@Getter
+@ToString
 public class Employee {
+
+    private int id;
+    private String name;
+    private String designation;
+    private String gender;
+    private double salary;
 }
