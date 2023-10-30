42c22b39d3a6dce77bbbfb8b0f33568cfa80ace3
Goutham <ggowthamsankar@gmail.com>
2023-10-30 18:15:51 +0530

    Update GroupingByMultipleFieldsExampleTest.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/java_8_streams/GroupingByMultipleFieldsExampleTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/java_8_streams/GroupingByMultipleFieldsExampleTest.java
@@ -1,5 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.dev.gmsk.java_8_streams;
+
+import in.dev.gmsk.jdbc.JDBCConnection;
+import in.dev.gmsk.model.Employee;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 class GroupingByMultipleFieldsExampleTest {
 
+    private static final String url = "jdbc:mysql://localhost:3306/dev_gmsk_db";
+    private static final String user = "root";
+    private static final String password = "root";
+    private static final JDBCConnection jdbcConnection;
+
+    static{
+        jdbcConnection = new JDBCConnection(url, user, password);
+    }
+
+    @Test
+    void groupByDesignationAndGenderTest() {
+
+        Map<String, Map<String, Long>> stringMapMap =
+                GroupingByMultipleFieldsExample.groupByDesignationAndGender(jdbcConnection);
+
+        System.out.println("stringMapMap = " + stringMapMap);
+    }
+
+    @Test
+    void groupByDesignationTest() {
+
+        Map<String, Long> stringLongMap =
+                GroupingByMultipleFieldsExample.groupByDesignation(jdbcConnection);
+
+        System.out.println("stringLongMap = " + stringLongMap);
+    }
+
+    @Test
+    void groupByMultipleFieldsCollectAggregatedTest() {
+
+        Map<String, Map<String, List<Employee>>> stringMapMap =
+                GroupingByMultipleFieldsExample.groupByMultipleFieldsCollectAggregated(jdbcConnection);
+
+        System.out.println("stringMapMap = " + stringMapMap);
+    }
 }
