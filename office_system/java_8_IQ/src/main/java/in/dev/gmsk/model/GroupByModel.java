c55aa55226910329292ddd32b5c9db3a8a1494cd
Goutham <ggowthamsankar@gmail.com>
2023-11-06 20:47:43 +0530

    Update GroupByModel.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/GroupByModel.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/GroupByModel.java
@@ -1,4 +1,16 @@
 package in.dev.gmsk.model;
 
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class GroupByModel {
+
+    private String designation;
+    private String gender;
 }
