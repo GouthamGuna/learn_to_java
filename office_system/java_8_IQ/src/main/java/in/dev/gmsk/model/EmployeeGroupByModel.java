85c147347963e5bb5015a32a287b82ec30a9065d
Goutham <ggowthamsankar@gmail.com>
2023-11-06 20:47:20 +0530

    Update EmployeeGroupByModel.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/EmployeeGroupByModel.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/EmployeeGroupByModel.java
@@ -1,4 +1,18 @@
 package in.dev.gmsk.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Setter
+@Getter
+@ToString
 public class EmployeeGroupByModel {
+
+    private int id;
+    private String name;
+    private long salary;
+    private GroupByModel groupBy;
 }
