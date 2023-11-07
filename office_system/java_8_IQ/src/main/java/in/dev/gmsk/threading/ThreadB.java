c3cbd6d7ad45b5f900afb64d4f02bf624ccf9bc9
Goutham <ggowthamsankar@gmail.com>
2023-11-07 14:53:52 +0530

    Update ThreadB.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/threading/ThreadB.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/threading/ThreadB.java
@@ -1,4 +1,25 @@
 package in.dev.gmsk.threading;
 
-public class ThreadB {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadB extends Thread{
+
+    List<Integer> someList = new ArrayList<>();
+
+    @Override
+    public void run() {
+
+        System.out.println("Child method = "+Thread.currentThread().getName());
+
+        synchronized (this) {
+            int total = 0;
+
+            for (int i = 0; i <= 100; i++) {
+                someList.add(total += i);
+            }
+
+            this.notify();
+        }
+    }
 }
