e2360be69cd2d0d22864c0c2a53328be67942e32
Goutham <ggowthamsankar@gmail.com>
2023-11-07 14:54:13 +0530

    Update SQL_QUERY_CONSTANTS.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/util/SQL_QUERY_CONSTANTS.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/util/SQL_QUERY_CONSTANTS.java
@@ -1,4 +1,6 @@
 package in.dev.gmsk.util;
 
-public class SQL_QUERY_CONSTRAION {
+public class SQL_QUERY_CONSTANTS {
+
+    public static final String FETCH_ALL_STUDENT_DETAILS = "SELECT `id`, CONCAT(`firstname`,' ',`lastname`) AS stuName, `gender`, `dateofbirth`, `studenturl`, `admissionno` FROM `student_registration`";
 }
