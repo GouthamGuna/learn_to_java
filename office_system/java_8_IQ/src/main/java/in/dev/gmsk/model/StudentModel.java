b2d31f8edce462349d158f2c7ca62e1b1617c73a
Goutham <ggowthamsankar@gmail.com>
2023-11-07 14:52:50 +0530

    Update StudentModel.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/StudentModel.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/model/StudentModel.java
@@ -1,4 +1,18 @@
 package in.dev.gmsk.model;
 
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentModel {
+
+    private String id;
+    private String name;
+    private String gender;
+    private String dob;
+    private String studentAvatar;
+    private String admissionNo;
 }
