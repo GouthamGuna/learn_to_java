5fb5c13be9ac2262be4157d6a0354fa8be44d6d3
Goutham <ggowthamsankar@gmail.com>
2023-11-07 14:54:40 +0530

    Update StudentPlayingThreadClassTest.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/threading/StudentPlayingThreadClassTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/threading/StudentPlayingThreadClassTest.java
@@ -1,5 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.dev.gmsk.threading;
 
-class StudentPlayingThreadClasTest {
+import in.dev.gmsk.jdbc.JDBCConnection;
+import in.dev.gmsk.model.StudentModel;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+class StudentPlayingThreadClassTest {
+
+    private static final String url = "jdbc:mysql://localhost:3306/krithvik";
+    private static final String user = "root";
+    private static final String password = "root";
+    private static final JDBCConnection jdbcConnection;
+
+    static {
+        jdbcConnection = new JDBCConnection(url, user, password);
+    }
+
+    @Test
+    void getAllStudentListTest() {
+
+        List<StudentModel> allStudentList =
+                StudentPlayingThreadClass.getAllStudentList(jdbcConnection);
+
+        System.out.println("allStudentList = " + allStudentList);
+    }
 }
