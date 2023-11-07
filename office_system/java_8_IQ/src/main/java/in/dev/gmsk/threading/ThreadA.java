1dd54cb235ebb3b807b611d73870e8cd3913e98b
Goutham <ggowthamsankar@gmail.com>
2023-11-07 14:53:34 +0530

    Update ThreadA.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/threading/ThreadA.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/threading/ThreadA.java
@@ -1,4 +1,18 @@
 package in.dev.gmsk.threading;
 
 public class ThreadA {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        System.out.println("Main method = "+Thread.currentThread().getName());
+
+        ThreadB threadB = new ThreadB();
+        threadB.start();
+
+        synchronized (threadB) {
+
+            threadB.wait(1000);
+            System.out.println("Output = " + threadB.someList);
+        }
+    }
 }
