46dff65d75619e6663bada0b72c180f35b590627
Goutham <ggowthamsankar@gmail.com>
2023-11-08 18:23:14 +0530

    Update InputFormat.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/String/InputFormat.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/String/InputFormat.java
@@ -1,4 +1,19 @@
 package in.dev.gmsk.String;
 
+import java.util.Scanner;
+
 public class InputFormat {
+
+    public void problemOne(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0; i<3; i++)
+        {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            //Complete this line
+            System.out.printf( "%-15s%03d %n", s1, x);
+        }
+        System.out.println("================================");
+    }
 }
