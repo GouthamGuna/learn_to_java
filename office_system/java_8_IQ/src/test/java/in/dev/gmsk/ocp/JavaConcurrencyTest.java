a36c5ebc523e49ec956b3b94b44ecc495a0f1bb7
Goutham <ggowthamsankar@gmail.com>
2023-11-10 16:22:08 +0530

    Update JavaConcurrencyTest.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/ocp/JavaConcurrencyTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/ocp/JavaConcurrencyTest.java
@@ -1,5 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.dev.gmsk.ocp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+// import static org.junit.jupiter.api.Assertions.*;
 
 class JavaConcurrencyTest {
 
+    @Test
+    void addValues() {
+
+        IntStream streamer = IntStream.of(40, 10, 6, 2, 60, 40, 600, 200, 500);
+
+        List<Integer> integers = JavaConcurrency.addValues(streamer);
+
+        System.out.println("integers = " + integers);
+    }
+
+    @Test
+    void muchBetterAddValues() {
+
+        IntStream streamer = IntStream.of(40, 10, 6, 2, 60, 40, 600, 200, 500);
+
+        List<Integer> integers = JavaConcurrency.muchBetterAddValues(streamer);
+
+        System.out.println("integers = " + integers);
+    }
 }
