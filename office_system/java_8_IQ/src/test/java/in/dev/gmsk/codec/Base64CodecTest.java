44b3dd244804d5fe7d5690e36a06752e81266584
Goutham <ggowthamsankar@gmail.com>
2023-11-15 17:24:10 +0530

    Update Base64CodecTest.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/codec/Base64CodecTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/codec/Base64CodecTest.java
@@ -1,5 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.dev.gmsk.codec;
+
+import org.junit.jupiter.api.Test;
+
+// import static org.junit.jupiter.api.Assertions.*;
 
 class Base64CodecTest {
 
+    @Test
+    void getAlphabet() {
+        char input = 'a';
+        byte[] alphabet = Base64Codec.getAlphabet(input);
+
+        System.out.println("alphabet = " + alphabet);
+    }
 }
