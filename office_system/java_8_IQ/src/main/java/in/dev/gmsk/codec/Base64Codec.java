7f1654e65f1fb80081d3aad675f94ff5b222ace0
Goutham <ggowthamsankar@gmail.com>
2023-11-15 17:23:36 +0530

    Update Base64Codec.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/codec/Base64Codec.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/codec/Base64Codec.java
@@ -1,4 +1,21 @@
 package in.dev.gmsk.codec;
 
 public class Base64Codec {
+
+    private final static byte[] _URL_SAFE_ALPHABET = {
+            (byte) 'A', (byte) 'B', (byte) 'C', (byte) 'D', (byte) 'E', (byte) 'F', (byte) 'G',
+            (byte) 'H', (byte) 'I', (byte) 'J', (byte) 'K', (byte) 'L', (byte) 'M', (byte) 'N',
+            (byte) 'O', (byte) 'P', (byte) 'Q', (byte) 'R', (byte) 'S', (byte) 'T', (byte) 'U',
+            (byte) 'V', (byte) 'W', (byte) 'X', (byte) 'Y', (byte) 'Z',
+            (byte) 'a', (byte) 'b', (byte) 'c', (byte) 'd', (byte) 'e', (byte) 'f', (byte) 'g',
+            (byte) 'h', (byte) 'i', (byte) 'j', (byte) 'k', (byte) 'l', (byte) 'm', (byte) 'n',
+            (byte) 'o', (byte) 'p', (byte) 'q', (byte) 'r', (byte) 's', (byte) 't', (byte) 'u',
+            (byte) 'v', (byte) 'w', (byte) 'x', (byte) 'y', (byte) 'z',
+            (byte) '0', (byte) '1', (byte) '2', (byte) '3', (byte) '4', (byte) '5',
+            (byte) '6', (byte) '7', (byte) '8', (byte) '9', (byte) '-', (byte) '_'
+    };
+
+    static byte[] getAlphabet(int options) {
+        return _URL_SAFE_ALPHABET;
+    }
 }
