95990a06d9b9775c23ed0d041c736f7e3c106cc8
Goutham <ggowthamsankar@gmail.com>
2023-11-22 13:30:41 +0530

    Update TCSInterviewTest1.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/tcs/TCSInterviewTest1.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/tcs/TCSInterviewTest1.java
@@ -1,4 +1,29 @@
 package in.dev.gmsk.tcs;
 
 public class TCSInterviewTest1 {
+
+     static int countingVowels(String values){
+        int count = 0;
+
+         if(0 > values.length()){
+             return count;
+         }
+
+         for(int i = 0; i < values.length(); i++){
+
+            char ch = values.charAt(i);
+
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static Long countingVowelsImplLambada(String values){
+
+        return values.chars().filter(i -> i == 'a' || i == 'e' || i == 'i'
+                || i == 'o' || i == 'u').count();
+    }
 }
