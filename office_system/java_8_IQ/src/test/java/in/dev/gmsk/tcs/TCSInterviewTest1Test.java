9a7e0fbddf83ba350e2364f5a2ac5218fbaa7c7f
Goutham <ggowthamsankar@gmail.com>
2023-11-22 13:31:00 +0530

    Update TCSInterviewTest1Test.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/tcs/TCSInterviewTest1Test.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/tcs/TCSInterviewTest1Test.java
@@ -1,5 +1,28 @@
+package in.dev.gmsk.tcs;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TCSInterviewTest1Test {
 
+    @Test
+    void testCountingVowels() {
+        String Values = "Gowtham Sankar";
+        assertNotNull(Values);
+
+        int i = TCSInterviewTest1.countingVowels(Values);
+
+        assertEquals(4, i);
+    }
+
+    @Test
+    void countingVowelsImplLambada() {
+        String Values = "String";
+        assertNotNull(Values);
+
+        Long aLong = TCSInterviewTest1.countingVowelsImplLambada(Values);
+
+        assertEquals(1, aLong);
+    }
 }
