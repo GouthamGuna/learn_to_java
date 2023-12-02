73fe8020a96f947b05e53db64552f98e80a95730
Goutham <ggowthamsankar@gmail.com>
2023-12-02 13:17:31 +0530

    Update InputFormatTest.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/String/InputFormatTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/String/InputFormatTest.java
@@ -1,5 +1,34 @@
+package in.dev.gmsk.String;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputFormatTest {
 
+    @Test
+    void testGenerateUUID() {
+
+        assertNotNull(InputFormat.generateUUID());
+        System.out.println(
+                InputFormat.generateUUID()
+        );
+    }
+
+    @Test
+    void testURLEncode() throws MalformedURLException, UnsupportedEncodingException {
+
+        String Base_URL = "http://gowthamguna.in";
+        String query_URL = "/login?userName=admin&password=admin123";
+
+        assertNotNull(Base_URL);
+        assertNotNull(query_URL);
+
+        System.out.println(
+                InputFormat.URLEncode(Base_URL+query_URL)
+        );
+    }
 }
