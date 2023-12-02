046e2b121be1589f25ad70baf302b0dbbd7458a1
Goutham <ggowthamsankar@gmail.com>
2023-12-02 13:16:55 +0530

    Update InputFormat.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/String/InputFormat.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/String/InputFormat.java
@@ -1,19 +1,34 @@
 package in.dev.gmsk.String;
 
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class InputFormat {
 
-    public void problemOne(){
+    public void problemOne() {
         Scanner sc = new Scanner(System.in);
         System.out.println("================================");
-        for(int i=0; i<3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             String s1 = sc.next();
             int x = sc.nextInt();
             //Complete this line
-            System.out.printf( "%-15s%03d %n", s1, x);
+            System.out.printf("%-15s%03d %n", s1, x);
         }
         System.out.println("================================");
+    }
+
+    static UUID generateUUID() {
+        return UUID.randomUUID();
+    }
+
+    static String URLEncode(String url) throws MalformedURLException, UnsupportedEncodingException {
+
+        URL urlInstance = new URL(URLEncoder.encode(url, "UTF-8"));
+
+        return urlInstance.toString();
     }
 }
