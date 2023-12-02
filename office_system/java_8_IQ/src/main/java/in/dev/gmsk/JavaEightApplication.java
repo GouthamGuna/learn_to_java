54b53bb2b77d73204d8e756462ab6cbc0f3f9b0d
Goutham <ggowthamsankar@gmail.com>
2023-12-02 13:16:17 +0530

    Update JavaEightApplication.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
@@ -2,6 +2,14 @@ package in.dev.gmsk;
 
 import in.dev.gmsk.abstraction.MyClass;
 import in.dev.gmsk.abstraction.MyInterface;
+import io.github.gaeqs.javayoutubedownloader.JavaYoutubeDownloader;
+import io.github.gaeqs.javayoutubedownloader.decoder.MultipleDecoderMethod;
+import io.github.gaeqs.javayoutubedownloader.stream.StreamOption;
+import io.github.gaeqs.javayoutubedownloader.stream.YoutubeVideo;
+import io.github.gaeqs.javayoutubedownloader.stream.download.StreamDownloader;
+
+import java.io.File;
+import java.util.Comparator;
 
 public class JavaEightApplication {
 
@@ -17,5 +25,25 @@ public class JavaEightApplication {
         System.out.println("output = " + output);
 
         System.out.println(object.methodD());
+    }
+
+    public static boolean download(String url, File folder) {
+        //Extracts and decodes all streams.
+        YoutubeVideo video = JavaYoutubeDownloader.decodeOrNull(url, MultipleDecoderMethod.AND, "html", "embedded");
+        //Gets the option with the greatest quality that has video and audio.
+        StreamOption option = video.getStreamOptions().stream()
+                .filter(target -> target.getType().hasVideo() && target.getType().hasAudio())
+                .min(Comparator.comparingInt(o -> o.getType().getVideoQuality().ordinal())).orElse(null);
+        //If there is no option, returns false.
+        if (option == null) return false;
+        //Prints the option type.
+        System.out.println(option.getType());
+        //Creates the file. folder/title.extension
+        File file = new File(folder, video.getTitle() + "." + option.getType().getContainer().toString().toLowerCase());
+        //Creates the downloader.
+        StreamDownloader downloader = new StreamDownloader(option, file, null);
+        //Runs the downloader.
+        new Thread(downloader).start();
+        return true;
     }
 }
