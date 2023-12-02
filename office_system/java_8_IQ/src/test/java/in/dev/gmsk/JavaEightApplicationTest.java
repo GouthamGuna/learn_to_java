e26fbae556ae2db9b08e7e292ecebf0c978bf6f8
Goutham <ggowthamsankar@gmail.com>
2023-12-02 13:17:16 +0530

    Update JavaEightApplicationTest.java

--- office_system/java_8_IQ/src/test/java/in/dev/gmsk/JavaEightApplicationTest.java
+++ office_system/java_8_IQ/src/test/java/in/dev/gmsk/JavaEightApplicationTest.java
@@ -2,6 +2,8 @@ package in.dev.gmsk;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaEightApplicationTest {
@@ -9,5 +11,19 @@ class JavaEightApplicationTest {
     @Test
     void mainTest() {
         assertTrue(true);
+    }
+
+    @Test
+    void testDownload(){
+
+        String URL = "https://youtu.be/d86ws7mQYIg?si=FdWenWOTqmpraetQ";// "https://youtu.be/zA8guDqfv40?si=JHDDikjqUkld_MfH";
+        File filePath = new File("file:///C:/Users/User/Downloads");
+
+        assertNotNull(URL);
+        assertNotNull(filePath);
+
+        // assertThrows(Exception.class, () -> JavaEightApplication.download(URL, filePath));
+
+        JavaEightApplication.download(URL, filePath);
     }
 }
