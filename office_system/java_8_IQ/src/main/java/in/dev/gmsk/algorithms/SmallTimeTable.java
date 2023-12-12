9b1c4c351b27e1f861c9253155cf8dc8c1ff7eb5
Goutham <ggowthamsankar@gmail.com>
2023-12-12 19:00:43 +0530

    update SmallTimeTable.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/algorithms/SmallTimeTable.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/algorithms/SmallTimeTable.java
@@ -1,4 +1,40 @@
 package in.dev.gmsk.algorithms;
 
+
+/**
+ * Here’s a sample Java code that implements a genetic algorithm to
+ * generate a timetable for a university 12:
+ *
+ *  https://stackoverflow.com/questions/31586199/university-timetable-scheduling-project-using-genetic-algorithm-java
+ *
+ * https://stackoverflow.com/questions/2304868/which-algorithm-to-use-for-generating-time-table-for-schools
+ * */
+
 public class SmallTimeTable {
+
+    /*public static void main (String [] args) {
+        //creating Subjects
+        Subject A = new Subject ("Human Computer Interaction", "Batman", 49);
+        Subject B = new Subject ("Artificial Intelligence", "Batman", 95);
+        Subject C = new Subject ("Human Computer Interaction", "Batman",180);
+        Subject D = new Subject ("Human Computer Interaction", "Batman", 20);
+        Subject E = new Subject ("Empty", "No-One", 0);
+
+        //Creating Class Venue
+        ClassVenu Class1 = new ClassVenu (100,"LecturerTheater1");
+        ClassVenu Class2 = new ClassVenu (50,"LecturerTheater2");
+        ClassVenu Class3 = new ClassVenu (200,"LecturerTheater3");
+
+        //Creating Days
+        Days Day1 =new Days (A, A, E, B, B, E, E, A, A);
+        Days Day2 =new Days (C, C, E, D, D, E, E, A, A);
+        Days Day3 =new Days (E, E, E, B, B, E, E, A, A);
+        Days Day4 =new Days (C, C, E, C, C, E, E, A, A);
+        Days Day5 =new Days (A, A, E, B, B, E, E, A, A);
+
+        //creating Timetable
+        TimeTable T1 = new TimeTable (Day1, Day2, Day3, Day4, Day5);
+        List<Subject> answer = T1.ShowTimetable (T1);
+        System.out.println (answer);
+    }*/
 }
