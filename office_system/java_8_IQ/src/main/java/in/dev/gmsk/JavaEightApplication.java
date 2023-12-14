6b0baf8a153adae2640aad110e8fae70cf039507
Goutham <ggowthamsankar@gmail.com>
2023-12-14 17:56:22 +0530

    Update JavaEightApplication.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/JavaEightApplication.java
@@ -1,5 +1,6 @@
 package in.dev.gmsk;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
@@ -31,6 +32,7 @@ public class JavaEightApplication {
         System.out.println(object.methodD());
 
         JavaEightApplication.moreCache();
+        JavaEightApplication.GuavaCacheTest();
     }
 
     public static boolean download(String url, File folder) {
@@ -70,6 +72,20 @@ public class JavaEightApplication {
 
     public static void GuavaCacheTest () {
 
+        Cache<String, String> cache = CacheBuilder.newBuilder().maximumSize(3).recordStats().build();
 
+        cache.put("1", "v1");
+        cache.put("2", "v2");
+        cache.put("3", "v3");
+        cache.put("4", "v4");
+
+        cache.getIfPresent("1");
+        cache.getIfPresent("2");
+        cache.getIfPresent("3");
+        cache.getIfPresent("4");
+        cache.getIfPresent("5");
+        cache.getIfPresent("6");
+
+        System.err.println("cache.stats() : "+cache.stats());
     }
 }
