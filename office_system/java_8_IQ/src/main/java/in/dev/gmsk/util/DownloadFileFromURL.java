08d203b9de55ac134ca427c8e7f569455b80d7a7
Goutham <ggowthamsankar@gmail.com>
2024-01-15 17:22:29 +0530

    Update DownloadFileFromURL.java

--- office_system/java_8_IQ/src/main/java/in/dev/gmsk/util/DownloadFileFromURL.java
+++ office_system/java_8_IQ/src/main/java/in/dev/gmsk/util/DownloadFileFromURL.java
@@ -1,4 +1,46 @@
 package in.dev.gmsk.util;
 
-public class JavaDownloadFileFromURL {
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class DownloadFileFromURL {
+
+    public static void main(String[] args) {
+        //String url = "https://www.journaldev.com/sitemap.xml";
+        String url = "https://vast.adsturn.com/laskdx.xml";
+
+        try {
+            downloadUsingNIO(url, "G:\\");
+
+            downloadUsingStream(url, "G:\\");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void downloadUsingStream(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count = 0;
+        while ((count = bis.read(buffer, 0, 1024)) != -1) {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+    }
+
+    private static void downloadUsingNIO(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+        rbc.close();
+    }
 }
