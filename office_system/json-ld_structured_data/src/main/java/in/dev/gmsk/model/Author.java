d5fbf47bd4b7f332851ddd3ce4fe26f2c815dae2
Goutham <ggowthamsankar@gmail.com>
2024-02-09 11:35:22 +0530

    Update Author.java

--- office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Author.java
+++ office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Author.java
@@ -4,6 +4,12 @@ import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * @author gowtham_sankar gunasekaran
+ * @see <a href="https://gouthamguna.github.io/in/">GMSK</a>
+ * @since 1.0
+ */
+
 @Setter
 @Getter
 public class Author {
