91d2c93d05c17377ac7ce41f7b73203f65dc5752
Goutham <ggowthamsankar@gmail.com>
2024-02-09 10:23:26 +0530

    Update Publisher.java

--- office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Publisher.java
+++ office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Publisher.java
@@ -1,5 +1,6 @@
 package in.dev.gmsk.model;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,10 @@ import java.util.List;
 @Setter
 @Getter
 public class Publisher {
+
+    @SerializedName("@type")
     public final String type = "Organization";
     public String name;
     public String url;
-    public List<String> logo;
+    public List<String> logos;
 }
