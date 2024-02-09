c8e63fdb193055dd0480e05ef0283fbd443ab069
Goutham <ggowthamsankar@gmail.com>
2024-02-09 11:36:15 +0530

    Update Publisher.java

--- office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Publisher.java
+++ office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Publisher.java
@@ -6,6 +6,12 @@ import lombok.Setter;
 
 import java.util.List;
 
+/**
+ * @author gowtham_sankar gunasekaran
+ * @see <a href="https://gouthamguna.github.io/in/">GMSK</a>
+ * @since 1.0
+ */
+
 @Setter
 @Getter
 public class Publisher {
