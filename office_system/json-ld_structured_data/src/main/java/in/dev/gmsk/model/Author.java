b69001bd45c7a2a2dd95f488ca86abd4032bfcdc
Goutham <ggowthamsankar@gmail.com>
2024-02-09 10:22:28 +0530

    Update Author.java

--- office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Author.java
+++ office_system/json-ld_structured_data/src/main/java/in/dev/gmsk/model/Author.java
@@ -1,11 +1,14 @@
 package in.dev.gmsk.model;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class Author {
+
+    @SerializedName("@type")
     public final String type = "Person";
     public String name;
     public String jobTitle;
