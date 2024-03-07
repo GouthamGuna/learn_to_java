5c906f7eb90250e3627126de22716a1c60c64f32
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-07 21:30:30 +0530

    Udate UpdateStockNewLogic.java

--- asus_system/cerp_util/src/main/java/in/dev/gmsk/tia/UpdateStockNewLogic.java
+++ asus_system/cerp_util/src/main/java/in/dev/gmsk/tia/UpdateStockNewLogic.java
@@ -5,28 +5,43 @@ public class UpdateStockNewLogic {
 
     public static void stockUpdatingCalculation(int actualProjectsQty, int actualMinimumQty, int poReceivedQty, int poUpdatedQty) {
 
+        int updatingMinQty;
+        int updatingProjectQty = 0;
+
         if (actualProjectsQty >= poUpdatedQty) {
 
             int remainingProjectQty = actualProjectsQty - poUpdatedQty;
-            int updatingProjectQty;
 
-            System.out.println( "remainingProjectQty = " + remainingProjectQty );
+            System.out.println( "ln: 15 remainingProjectQty = " + remainingProjectQty );
 
             if (remainingProjectQty >= poReceivedQty) {
 
                 updatingProjectQty = poReceivedQty;
-                System.out.println( "updatingProjectQty = " + updatingProjectQty );
+                System.out.println( "ln: 20 updatingProjectQty = " + updatingProjectQty );
             } else {
 
                 updatingProjectQty = poReceivedQty - remainingProjectQty;
                 poReceivedQty = poReceivedQty - updatingProjectQty;
+                System.out.println( "ln: 25 updatingProjectQty = " + updatingProjectQty );
+                System.out.println( "ln: 26 poReceivedQty = " + poReceivedQty );
             }
         }
 
-        if (poReceivedQty != 0) {
-            int remainingMinmiumQty = actualProjectsQty + actualMinimumQty - poUpdatedQty;
+        int actualProMinQty = actualProjectsQty + actualMinimumQty;
+        System.out.println( "ln: 31 actualProMinQty = " + actualProMinQty );
 
-            System.out.println( "remainingMinmiumQty = " + remainingMinmiumQty );
+        if (actualProMinQty >= poUpdatedQty && poReceivedQty != 0) {
+            int remainingMinmiumQty = actualProMinQty - updatingProjectQty - poUpdatedQty;
+
+            if (remainingMinmiumQty >= poReceivedQty) {
+                updatingMinQty = poReceivedQty;
+                System.out.println( "ln: 38 If updatingMinQty = " + updatingMinQty );
+            } else {
+                updatingMinQty = poReceivedQty - remainingMinmiumQty;
+                System.out.println( "ln: 41 Else updatingMinQty = " + updatingMinQty );
+            }
+
+            System.out.println( "ln: 44 remainingMinmiumQty = " + remainingMinmiumQty );
         }
     }
 }
