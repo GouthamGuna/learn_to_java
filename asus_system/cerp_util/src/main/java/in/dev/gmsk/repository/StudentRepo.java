46071a91929aee7a4fec84eca7a123c152868466
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-12 03:48:33 +0530

    Update StudentRepo.java

--- asus_system/cerp_util/src/main/java/in/dev/gmsk/repository/StudentRepo.java
+++ asus_system/cerp_util/src/main/java/in/dev/gmsk/repository/StudentRepo.java
@@ -1,2 +1,50 @@
-package in.dev.gmsk.repository;public class StudentRepo {
+package in.dev.gmsk.repository;
+
+import in.dev.gmsk.model.Students;
+import in.dev.gmsk.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StudentRepo {
+
+    public String saveStudents(Students student) {
+
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // save the student objects
+            session.persist( student );
+
+            // commit transaction
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace( System.out );
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+
+        return null;
+    }
+
+    public Stream<Students> getAllStudents() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            List<Students> students = session.createQuery( "from Students", Students.class ).list();
+
+            return students.parallelStream();
+
+        } catch (Exception e) {
+            e.printStackTrace( System.out );
+            return null;
+        }
+    }
 }
