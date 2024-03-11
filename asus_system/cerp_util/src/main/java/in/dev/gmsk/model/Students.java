b6f3fa1d34c81dd485b65549706383f8196c8e07
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-12 03:49:42 +0530

    Update -all

---
+++ asus_system/cerp_util/src/main/java/in/dev/gmsk/model/Students.java
@@ -0,0 +1,47 @@
+package in.dev.gmsk.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Students {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String rollNo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Students \n" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rollNo='" + rollNo + '\'';
+    }
+}
