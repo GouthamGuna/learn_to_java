bbb315d84fb1a0d27eb590224462d72d7c62cc76
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-12 03:49:24 +0530

    Update StudentControllerTest.java

--- asus_system/cerp_util/src/test/java/in/dev/gmsk/controller/StudentControllerTest.java
+++ asus_system/cerp_util/src/test/java/in/dev/gmsk/controller/StudentControllerTest.java
@@ -1,4 +1,27 @@
+package in.dev.gmsk.controller;
+
+import in.dev.gmsk.model.Students;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StudentControllerTest {
-  
+    private static final StudentController CONTROLLER = new StudentController();
+
+    @Test
+    void TestSaveStudent() {
+
+        Students students = new Students();
+        students.setName( "Manoj N" );
+        students.setRollNo( "16EE507" );
+        assertNotNull( students );
+        String s = CONTROLLER.saveStudent( students );
+        System.out.println( "s = " + s );
+    }
+
+    @Test
+    void getAllStudentsList() {
+        CONTROLLER.getAllStudentsList().toList().forEach( System.out::println );
+        assertTrue( true );
+    }
 }
