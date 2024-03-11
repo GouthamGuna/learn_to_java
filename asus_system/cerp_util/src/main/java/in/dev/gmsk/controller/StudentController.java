a6e2444cbe53baa110cafdfa2aedfa84c1dfa32f
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-12 03:48:09 +0530

    Update StudentController.java

--- asus_system/cerp_util/src/main/java/in/dev/gmsk/controller/StudentController.java
+++ asus_system/cerp_util/src/main/java/in/dev/gmsk/controller/StudentController.java
@@ -1,2 +1,18 @@
-package in.dev.gmsk.controller;public class StudentController {
+package in.dev.gmsk.controller;
+
+import in.dev.gmsk.model.Students;
+import in.dev.gmsk.service.StudentServiceImpl;
+
+import java.util.stream.Stream;
+
+public class StudentController {
+    private static final StudentServiceImpl STUDENT_SERVICE = new StudentServiceImpl();
+
+    public String saveStudent(Students student) {
+        return STUDENT_SERVICE.saveStudentData( student );
+    }
+
+    public Stream<Students> getAllStudentsList() {
+        return STUDENT_SERVICE.getAllStudentsList();
+    }
 }
