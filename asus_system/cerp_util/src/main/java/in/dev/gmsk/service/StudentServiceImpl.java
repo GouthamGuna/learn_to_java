1aa3444b7b6ee01651e4faf6dcfed09ca1ed17df
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-12 03:49:03 +0530

    Update StudentServiceImpl.java

--- asus_system/cerp_util/src/main/java/in/dev/gmsk/service/StudentServiceImpl.java
+++ asus_system/cerp_util/src/main/java/in/dev/gmsk/service/StudentServiceImpl.java
@@ -1,2 +1,19 @@
-package in.dev.gmsk.service;public class StudentServiceImpl {
+package in.dev.gmsk.service;
+
+import in.dev.gmsk.model.Students;
+import in.dev.gmsk.repository.StudentRepo;
+
+import java.util.stream.Stream;
+
+public class StudentServiceImpl {
+
+    private static final StudentRepo STUDENT_REPO = new StudentRepo();
+
+    public String saveStudentData(Students student) {
+        return STUDENT_REPO.saveStudents( student );
+    }
+
+    public Stream<Students> getAllStudentsList() {
+        return STUDENT_REPO.getAllStudents();
+    }
 }
