f7e8bd7daf70d32214fef50c9806f9645f99f8e2
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-26 07:28:52 +0530

    Update StudentServiceImpl.java

--- asus_system/cerp_util/src/main/java/in/dev/gmsk/service/StudentServiceImpl.java
+++ asus_system/cerp_util/src/main/java/in/dev/gmsk/service/StudentServiceImpl.java
@@ -3,6 +3,8 @@ package in.dev.gmsk.service;
 import in.dev.gmsk.model.Students;
 import in.dev.gmsk.repository.StudentRepo;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 public class StudentServiceImpl {
@@ -10,10 +12,33 @@ public class StudentServiceImpl {
     private static final StudentRepo STUDENT_REPO = new StudentRepo();
 
     public String saveStudentData(Students student) {
-        return STUDENT_REPO.saveStudents( student );
+        return STUDENT_REPO.saveStudents(student);
     }
 
     public Stream<Students> getAllStudentsList() {
         return STUDENT_REPO.getAllStudents();
     }
+
+    /**
+     * @Pattern(regexp = "\\A(?!\\s*\\Z).+")
+     */
+
+    public String getStudentAvatar(int studentId) {
+
+        ConcurrentHashMap<Integer, String> tableOne = new ConcurrentHashMap<>();
+        tableOne.put(1, "");
+        tableOne.put(2, "(NULL)");
+        tableOne.put(3, "null");
+        tableOne.put(4, " ");
+        tableOne.put(5, "xyz");
+        tableOne.put(6, "/SchoolInfo/StudentAvatar/2023-2024/xyz_6.png");
+        tableOne.put(7, "/SchoolInfo/StudentAvatar/2023-2024/xyz_7.png");
+
+        return isaBoolean.apply(studentId, tableOne) ? "(NULL)" : tableOne.get(studentId);
+    }
+
+    private final BiFunction<Integer, ConcurrentHashMap<Integer, String>, Boolean> isaBoolean = (studentId, tableOne) ->
+            tableOne.get(studentId).isEmpty() || (tableOne.get(studentId).isBlank() &&
+                    (null != tableOne.get(studentId)) && tableOne.get(studentId).equalsIgnoreCase(" "));
+
 }
