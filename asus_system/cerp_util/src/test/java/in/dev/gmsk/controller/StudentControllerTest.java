e31263ffe8764aee0a0f1e08bc1fddcaabdcfd16
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-30 08:16:28 +0530

    Update StudentControllerTest.java

--- asus_system/cerp_util/src/test/java/in/dev/gmsk/controller/StudentControllerTest.java
+++ asus_system/cerp_util/src/test/java/in/dev/gmsk/controller/StudentControllerTest.java
@@ -3,6 +3,8 @@ package in.dev.gmsk.controller;
 import in.dev.gmsk.model.Students;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentControllerTest {
@@ -12,25 +14,30 @@ class StudentControllerTest {
     void TestSaveStudent() {
 
         Students students = new Students();
-        students.setName( "Manoj N" );
-        students.setRollNo( "16EE507" );
-        assertNotNull( students );
-        String s = CONTROLLER.saveStudent( students );
-        System.out.println( "s = " + s );
+        students.setName("Manoj N");
+        students.setRollNo("16EE507");
+        assertNotNull(students);
+        String s = CONTROLLER.saveStudent(students);
+        System.out.println("s = " + s);
     }
 
     @Test
     void getAllStudentsList() {
-        CONTROLLER.getAllStudentsList().toList().forEach( System.out::println );
-        assertTrue( true );
+        CONTROLLER.getAllStudentsList().toList().forEach(System.out::println);
+        assertTrue(true);
     }
 
     @Test
-    void testGetStudentAvatar(){
+    void testGetStudentAvatar() {
         String expected = "(NULL)";
         String actual = CONTROLLER.getStudentAvatar(1);
 
         assertEquals(expected, actual);
         System.out.println("studentAvatarPath = " + actual);
+    }
+
+    @Test
+    void testComposeMultiMethodOutput() throws ExecutionException, InterruptedException {
+        CONTROLLER.composeMultiMethodOutput();
     }
 }
